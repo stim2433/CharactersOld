af577a8c0d8b1be9ebfb298ac1094f42bed5b933
stimLite <gross2433@gmail.com>
2022-06-05 13:38:04 +0300

    add table view

--- Characters/UI/CharactersView/View/CharactersView.swift
+++ Characters/UI/CharactersView/View/CharactersView.swift
@@ -7,17 +7,57 @@
 
 import UIKit
 
+
+//MARK: Character veiw controller
 class CharacterViewController: UIViewController {
     
     var presenter: CharacterViewPresenterProtocol!
+    let cellId = "cellIdCharacters"
+    
+    //pre config ui table view
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.frame = .zero
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.addSubview(tableView)
+        
+        
+        //subscribe datasource and delegate
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //register cell in table view
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
 }
 
+//MARK: extension table view datasource
+extension CharacterViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = .green
+        
+        return cell
+    }
+}
+
+//MARK: extension table view delegate
+extension CharacterViewController: UITableViewDelegate {
+    
+}
+
+//MARK: Subscration Character view protocol
 extension CharacterViewController: CharacterViewProtocol {
     func success() {
         print ("hi, I'm is live")
